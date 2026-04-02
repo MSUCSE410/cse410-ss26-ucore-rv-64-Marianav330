3baa41061736b8156d95bf1387df6f9a882f28da
Mariana Vangelov <131295956+Marianav330@users.noreply.github.com>
2026-04-02 16:47:05 +0000

    Project 2 Submission

--- os/proc.h
+++ os/proc.h
@@ -6,6 +6,8 @@
 
 #define NPROC (16)
 
+#define MAX_SYSCALL_NUM 500
+
 // Saved registers for kernel context switches.
 struct context {
 	uint64 ra;
@@ -28,6 +30,20 @@ struct context {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+
+typedef enum {
+	UnInit,
+	Ready,
+	Running,
+	Exited,
+} TaskStatus;
+
+typedef struct {
+	TaskStatus status;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
+} TaskInfo;
+
 // Per-process state
 struct proc {
 	enum procstate state; // Process state
@@ -38,10 +54,17 @@ struct proc {
 	struct trapframe *trapframe; // data page for trampoline.S
 	struct context context; // swtch() here to run process
 	uint64 max_page;
-	/*
-	* LAB1: you may need to add some new fields here
-	*/
+	
+	unsigned int syscall_times[MAX_SYSCALL_NUM]; //array storing amount of syscall ID calls
+	uint64 start_time; // stores CPU cycle count from when process starts
+	
+
 };
+
+
+
+
+
 
 /*
 * LAB1: you may need to define struct for TaskInfo here
--- os/syscall.c
+++ os/syscall.c
@@ -4,6 +4,7 @@
 #include "syscall_ids.h"
 #include "timer.h"
 #include "trap.h"
+#include "proc.h"
 
 uint64 sys_write(int fd, uint64 va, uint len)
 {
@@ -34,15 +35,22 @@ uint64 sys_sched_yield()
 
 uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofday in pagetable. (VA to PA)
 {
-	// YOUR CODE
-	val->sec = 0;
-	val->usec = 0;
+	if (!val){
+		return -1;
+	}
 
+	//create the time result here in kernel since val is a user VA and we cannot write to it
 	/* The code in `ch3` will leads to memory bugs*/
+	TimeVal valTemp;
+    uint64 cycle = get_cycle();
+    valTemp.sec = cycle / CPU_FREQ;
+    valTemp.usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
 
-	// uint64 cycle = get_cycle();
-	// val->sec = cycle / CPU_FREQ;
-	// val->usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+	//copys from kernel to page table 
+    if (copyout(curr_proc()->pagetable, (uint64)val, (char *)&valTemp, sizeof(TimeVal)) < 0) {
+        return -1;
+    }
+
 	return 0;
 }
 
@@ -52,11 +60,145 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofd
 /*
 * LAB1: you may need to define sys_task_info here
 */
+uint64 sys_task_info( TaskInfo *task_info) {
+    struct proc *p = curr_proc();
+
+    if (task_info == 0) return -1; 
+
+    // Populate a temporary TaskInfo struct in kernel space
+    TaskInfo temp;
+
+    temp.status = Running;
+
+    // Add the syscall counts to our task info struct
+    for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+        temp.syscall_times[i] = p->syscall_times[i];
+    }
+
+	temp.time = (int)((get_cycle() - p->start_time) / (CPU_FREQ / 1000));
+    // calculate then set the time for task info
+	//copy out to send data from kernel to va
+    if (copyout(p->pagetable, (uint64)task_info, (char *)&temp, sizeof(TaskInfo)) < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int sys_mmap(void* start, unsigned long long len, int port, int flag, int fd){
+	// Cast the void* start address to a uint64 for pointer arithmetic
+	uint64 virtAdrStart = (uint64) start;
+
+	// Reject if start address is not page-aligned
+	if (virtAdrStart % PAGE_SIZE != 0){
+		return -1;
+	}
+
+	// Reject if any bits above the lower 3 (R/W/X) are set in port which makes them invalid permission flags
+	if ((port & ~0x7)!=0){
+		return -1;
+	}
+
+	// Reject if all permission bits are zero to make sure its accessible
+	if((port & 0x7) == 0){
+		return -1;
+	}
+
+	// Reject if the requested length exceeds 1 GiB
+	if (len > (1UL << 30)){
+		return -1;
+	}
+
+	// Round len up to the nearest page boundary
+	uint64 size = PGROUNDUP(len);
+
+	// Calculate the exclusive end address of the virtual range to be mapped
+	uint64 virtAdrEnd = virtAdrStart + size;
+
+	// Get a pointer to the current process's struct
+	struct proc *p = curr_proc(); 
+
+	// Walk every page in the requested range and reject if any are already mapped
+	for (uint64 va = virtAdrStart; va < virtAdrEnd; va+= PAGE_SIZE){
+		if (walkaddr(p->pagetable, va)!=0){return -1;}
+	}
+
+	// Start with PTE_U 
+	int permBits = PTE_U;
+
+	// Bit 0 of port the read permission
+	if (port & 0x1){
+		permBits = permBits | PTE_R;
+	}
+
+	// Bit 1 of port the write permission
+	if (port & 0x2){
+		permBits = permBits | PTE_W;
+	}
+
+	// Bit 2 of port the execute permission
+	if (port & 0x4){
+		permBits = permBits | PTE_X;
+	}
+
+	// Allocate and map one physical page for each virtual page in the range
+	for (uint64 va = virtAdrStart; va < virtAdrEnd; va+= PAGE_SIZE){
+		// Allocate a fresh physical page from the kernel's free memory pool
+		void* physAdr = kalloc();
+
+		// If kalloc returns NULL, the system is out of memory
+		if (!physAdr){return -1;}
+
+		// Zero out the page so no leftover kernel data is leaked to userspace
+		memset(physAdr, 0, PAGE_SIZE);
+
+		// Install the virtual physical mapping in the process's page table with the assembled permission bits
+		if (mappages(p->pagetable, va, PAGE_SIZE, (uint64)physAdr, permBits) < 0){return -1;}
+	}
+	return 0;
+}
+
+
+int sys_munmap(void* start, unsigned long long len){
+	// Cast the void* start address to a uint64 for pointer arithmetic
+	uint64 virtAdrStart = (uint64) start;
+
+	// Nothing to unmap no need to unmap
+	if (len == 0){return 0;}
+
+	// Reject if start address is not page-aligned
+	if (virtAdrStart %PAGE_SIZE != 0){
+		return -1;
+	}
+
+	// Round len up to the nearest page boundary to cover the full range
+	uint64 size = PGROUNDUP(len);
+
+	// Calculate the exclusive end address of the virtual range to be unmapped
+	uint64 virtAdrEnd = virtAdrStart + size;
+
+	// Get a pointer to the current process's struct
+	struct proc *p = curr_proc();
+	
+	// Walk every page in the range and reject if any are NOT mapped 
+	for (uint64 va = virtAdrStart; va < virtAdrEnd; va+= PAGE_SIZE){
+		if (walkaddr(p->pagetable, va) ==0){return -1;}
+	}
+
+	// Calculate number of pages to unmap
+	uint64 npages = size/PAGE_SIZE;
+
+	// Remove the mappings from the page table and free the underlying physical pages
+	// (the '1' argument is the do_free flag — tells uvmunmap to call kfree on each page)
+	uvmunmap(p->pagetable, virtAdrStart, npages, 1);
+	return 0;
+}
 
 extern char trap_page[];
 
 void syscall()
 {
+	struct proc *p = curr_proc();
 	struct trapframe *trapframe = curr_proc()->trapframe;
 	int id = trapframe->a7, ret;
 	uint64 args[6] = { trapframe->a0, trapframe->a1, trapframe->a2,
@@ -66,6 +208,10 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	//update syscall counter for task info here
+	if ( id >= 0 && id < MAX_SYSCALL_NUM ) {
+    	p->syscall_times[id]++;
+	}
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], args[1], args[2]);
@@ -82,6 +228,16 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	//add SYS_taskinfo case here
+	case SYS_task_info:
+        ret = sys_task_info((TaskInfo *)args[0]);
+        break;
+	case SYS_mmap:
+		ret = sys_mmap((void *)args[0], (unsigned long long)args[1], (int)args[2], (int)args[3], (int)args[4]);
+		break;
+	case SYS_munmap:
+		ret = sys_munmap((void *)args[0], (unsigned long long)args[1]);
+		break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
